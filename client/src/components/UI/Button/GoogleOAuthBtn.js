f62d901ffc029a74df9b11e1f3fa3627c9c8de98
jithendaraa <jithen.subra@gmail.com>
2019-04-05 23:17:09 +0530

    googleOAuth server side done

---
+++ client/src/components/UI/Button/GoogleOAuthBtn.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+// import classes from './Button.css';
+
+import Aux from '../../../hoc/Auxx/Auxx';
+import Glogo from '../../../assets/logos/googleLogo.jpg';
+import Button from '@material-ui/core/Button';
+
+
+const googleOAuthBtn = (props) => {
+
+
+    let btnStyle = {
+        width: "310px",
+        height: "90px",
+        backgroundColor: 'white',
+        textAlign: 'center',
+        cursor: 'pointer',
+        borderRadius: "10px",
+        border: "none",
+        borderShadow: "none"
+    };
+
+    let imgStyle = {
+        width: "50px",
+        height: "50px",
+        borderRadius: "5px",
+        float: "left",
+        paddingTop: "15px"
+    };
+
+    let bstyle = {
+        fontSize: "20px",
+        paddingLeft: "0px"
+    };
+
+    let astyle = {
+        color: 'black',
+        textDecoration: "none"
+    };
+
+
+    return (
+        <Aux>
+            <Button variant="contained" color="white" style={btnStyle}>
+                <a href='/auth/google' style={astyle}>
+                    <img src={Glogo} alt="img" style={imgStyle} />
+                    <p><b style={bstyle}>  Signup with Google</b></p>
+                </a>
+            </Button>
+            
+        </Aux>
+    );
+}
+
+export default googleOAuthBtn;
--- server.js
+++ server.js
@@ -1,17 +1,30 @@
 const express = require('express');
-const http = require('http');
+require('./services/passport');
 const mongoose = require('mongoose');
+const http = require('http');
 const cookieSession = require('cookie-session');
 const bodyParser = require('body-parser');
-const passport = require('passport');
-
-const PORT = process.env.PORT || 5000;
 
 let app = express();
 
-app.use(express.static(__dirname + 'public'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+require('./routes/authRoutes')(app);
+
+
+
+
+
+
+
+// app.use(express.static(__dirname + 'public'));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+//Client ID: 303008842237-q4d50j6334a0i0221ups5dpegbigpv9u.apps.googleusercontent.com
+//Client Secret eu5t2rzniw5E00h5wWVGJb4L
+
+
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log("listening on port 5000 -- server");
